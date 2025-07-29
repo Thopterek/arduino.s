4fb1097559c290d179c4944aa8238fc72cb91245
Nataniel Dziadzia <ndziadzi@2-G-6.42heilbronn.de>
2025-07-29 11:48:08 +0200

    starting with digital write

---
+++ src/digital_write.s
@@ -0,0 +1,31 @@
+# pre assumption for writing
+.intel_syntax noprefix
+
+# main function to be called
+.global digital_write
+
+# support function for the first vars
+.global digital_pin_to_timer
+.global digital_pin_to_bit_mask
+.global digital_pin_to_port
+
+# actual implementation
+# first argument rdi, second rsi
+digital_write:
+	xor r8, r8
+	xor r9, r9
+	xor r10, r10
+	call digital_pin_to_timer
+	mov r8, rax
+	call digital_pin_to_bit_mask
+	mov r9, rax
+	call digital_pin_to_port
+	mov r10, rax
+	cmp r10, 0
+	je done
+
+done:
+	xor rax, rax
+	ret
+
+digital_pin_to_timer:
